bc345514f70a8acb2f0176d79454f1299e401b07
roger <roger.tuyishime@gmail.com>
2022-09-23 10:29:04 +0200

    initialize a new project

---
+++ src/main/java/com/example/pinkcity/PinkCityApplication.java
@@ -0,0 +1,13 @@
+package com.example.pinkcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinkCityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinkCityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pinkcity/PinkCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pinkcity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinkCityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
