2d36f80a7d9f2665dc86a67ee2439b1187a47010
roger <roger.tuyishime@gmail.com>
2022-09-23 11:07:04 +0200

    Complete CoffeeBar model

--- src/main/java/com/example/pinkcity/a_models/CoffeeBar.java
+++ src/main/java/com/example/pinkcity/a_models/CoffeeBar.java
@@ -1,8 +1,61 @@
 package com.example.pinkcity.a_models;
 
+/**
+ * cartography of a CoffeeBar
+ */
 public class CoffeeBar {
     private String id;
     private String name;
     private String streetName;
-    private String postalCode;
+    private Integer postalCode;
+    private String phoneNumber;
+    private String WebSiteUrl;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public Integer getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(Integer postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getWebSiteUrl() {
+        return WebSiteUrl;
+    }
+
+    public void setWebSiteUrl(String webSiteUrl) {
+        WebSiteUrl = webSiteUrl;
+    }
 }
